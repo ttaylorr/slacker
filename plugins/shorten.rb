47dbc6132f300376f41bfcd5fb4dc3bffb999b5e
Taylor Blau <me@ttaylorr.com>
2014-05-14 13:59:55 -0400

    Add goo.gl link shortener

---
+++ plugins/shorten.rb
@@ -0,0 +1,27 @@
+require 'uri'
+require 'googl'
+
+module Slacker
+  class Shorten < Plugin
+
+    @@responses = [
+      'All done!',
+      'Easy-peasy!',
+      'Here it is:',
+      'No problem!'
+    ]
+
+    def pattern
+      /shorten/
+    end
+
+    def respond (text, user_name, channel_name, timestamp)
+      longUrl = text[text.index('http')..text.length()]
+      googl = Googl.shorten(longUrl)
+
+      return @@responses.sample + ' ' + googl.short_url 
+    end
+
+    Bot.register(Shorten)
+  end
+end
