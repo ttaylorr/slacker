7c01678f7b5cb6f1d0c91df1408fc85877ed452d
Taylor Blau <me@ttaylorr.com>
2014-05-14 17:04:25 -0400

    Add Google Images implementation

---
+++ plugins/images.rb
@@ -0,0 +1,20 @@
+require 'google-search'
+
+module Slacker
+  class Images < Plugin
+
+    def pattern
+      /image(\s?me)?/
+    end
+
+    def respond (text, user_name, channel_name, timestamp)
+      address = pattern.match(text)
+      query = text[address.end(0)..text.length()]
+
+      image = Google::Search::Image.new(:query => query).first
+      return image.uri
+    end
+
+    Bot.register(Images)
+  end
+end
