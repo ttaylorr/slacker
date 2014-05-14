c89226a7f278b87c277a5e6d48e374ec95aecc13
Taylor Blau <me@ttaylorr.com>
2014-05-14 19:10:04 -0400

    deal with it!

---
+++ plugins/dealwithit.rb
@@ -0,0 +1,48 @@
+module Slacker
+  class DealWithIt < Plugin
+
+    @@deals = [
+      "http://i.imgur.com/ykDuU.gif",
+      "http://i.imgur.com/3PWHn.gif",
+      "http://i.imgur.com/sEinL.gif",
+      "http://i.imgur.com/zsK7e.gif",
+      "http://i.imgur.com/rE2aH.gif",
+      "http://i.imgur.com/Wj3Do.gif",
+      "http://i.imgur.com/SvdS0.gif",
+      "http://i.imgur.com/bh6xc.gif",
+      "http://i.imgur.com/aoaqO.gif",
+      "http://i.imgur.com/oe01J.gif",
+      "http://i.imgur.com/N5N4x.gif",
+      "http://i.imgur.com/Z3KIP.gif",
+      "http://i.imgur.com/oFo42.gif",
+      "http://i.imgur.com/rbBAaRs.gif",
+      "http://i.imgur.com/JDRZ21o.gif",
+      "http://i.imgur.com/WhiQ67r.gif",
+      "http://i.imgur.com/KLLX1xx.png",
+      "http://i.imgur.com/BjtEpbY.gif",
+      "http://i.imgur.com/Z3DJlxS.gif",
+      "http://i.imgur.com/qjTTrcB.gif",
+      "http://i.imgur.com/HHyy9RL.gif",
+      "http://i.imgur.com/PilYVTI.gif",
+      "http://i.imgur.com/i6xvVSN.gif",
+      "http://i.imgur.com/5mnJ0je.gif",
+      "http://i.imgur.com/myOz11I.gif",
+      "http://i.imgur.com/VyKt1xD.gif",
+      "http://i.imgur.com/tBGAWgf.gif",
+      "http://i.imgur.com/2MTQrQf.gif",
+      "http://i.imgur.com/CxhomG0.gif",
+      "http://i.imgur.com/vA2UnyC.gif",
+      "http://i.imgur.com/5pMiY03.gif"
+    ]
+
+    def pattern
+      /deal\s?with\s?it/
+    end
+
+    def respond (text, user_name, channel_name, timestamp)
+      @@deals.sample
+    end
+
+    Bot.register(DealWithIt)
+  end
+end
