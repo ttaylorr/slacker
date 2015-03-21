93760e1b66e7a69f11391d7f7aad94300e5c07ae
Taylor Blau <me@ttaylorr.com>
2015-03-20 21:22:07 -0500

    Implement Jiralicious with the JiraPlugin

--- lib/plugins/jira/jira_plugin.rb
+++ lib/plugins/jira/jira_plugin.rb
@@ -1,35 +1,70 @@
 require_relative '../plugin'
 
+require 'dotenv'
+require 'jiralicious'
+
 module Slacker
   module Plugins
     class JiraPlugin < Plugin
+      def initialize
+        Jiralicious.configure do |config|
+          config.username = ENV["JIRA_USERNAME"]
+          config.password = ENV["JIRA_PASSWORD"]
+          config.uri = ENV["JIRA_URL"]
+          config.api_version = "latest"
+          config.auth_type = :basic
+        end
+
+        @jira = Jiralicious
+      end
+
       def ready(robot)
         robot.respond /i am (.*) on jira/i do |message, match|
           username = match.captures[0]
-
-          set_jira_username(robot, message.user, username)
-          message << "OK, you are *#{username}* on JIRA"
+          handle_username_change(username, message, robot)
         end
 
         robot.respond /my\sissues/ do |message|
-          jira_name = get_jira_username(robot, message.user)
+          jira_opts = {
+            :username => get_jira_username(robot, message.user),
+            :project => ENV["JIRA_DEFAULT_PROJECT"]
+          }
 
-          if jira_name.nil?
+          if jira_opts[:username].nil?
             message << "What's your JIRA username, again?"
-            message.expect_reply /.*/ do |reply, match|
-              jira_name = match
 
-              set_jira_username(robot, message.user, jira_name)
-              write_jira_issues_to(reply, jira_name)
+            message.expect_reply /.*/ do |reply, match|
+              handle_username_change(match, reply, robot)
+
+              jira_opts[:username] = match
+
+              write_jira_issues_to(reply, jira_opts)
             end
           else
-            write_jira_issues_to(message, jira_name)
+            write_jira_issues_to(message, jira_opts)
           end
         end
       end
 
-      def write_jira_issues_to(message, jira_username)
-        message << "Gathering issues for #{jira_username}"
+      def handle_username_change(username, message, robot)
+        begin
+          jira_user = @jira::User.find(username)
+          set_jira_username(robot, message.user, jira_user.name)
+
+          message << "OK, you are *#{jira_user.name}* on JIRA"
+        rescue Exception => e
+          message << "I couldn't find a user named #{username} on JIRA :disappointed:"
+        end
+      end
+
+      def write_jira_issues_to(message, opts)
+        issues_query = "PROJECT=\"#{opts[:project]}\" AND assignee in (#{opts[:username]}) AND status=Open"
+        response = @jira.search(issues_query)
+
+        message << "Looks like you have #{response.issues.length} issues assigned to you:"
+        response.issues.each do |issue|
+          message << "*#{issue.jira_key}* - #{issue.summary}"
+        end
       end
 
       private
