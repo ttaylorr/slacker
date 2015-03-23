f690256437bbf04c062f1882f99d04ada8030e2b
Taylor Blau <me@ttaylorr.com>
2015-03-23 15:20:52 -0400

    Add the graph search plugin

--- lib/plugins/graphite/graphite_ensemble.rb
+++ lib/plugins/graphite/graphite_ensemble.rb
@@ -1,11 +1,18 @@
 require_relative '../plugin'
+
+require_relative './graphite_api.rb'
+
 require_relative './graph_view_plugin.rb'
+require_relative './graph_search_plugin.rb'
 
 module Slacker
   module Plugins
     class GraphiteEnsemble < Plugin
       def ready(robot)
+        graphite_api = GraphiteAPI.new(ENV["GRAPHITE_API_HOST"], ENV["GRAPHITE_API_PORT"])
+
         robot.plug(GraphViewPlugin.new)
+        robot.plug(GraphSearchPlugin.new(graphite_api))
       end
     end
   end
