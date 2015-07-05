18538a4e1ab1741af27e3e613150263b3764836c
Taylor Blau <me@ttaylorr.com>
2015-07-05 18:26:00 -0400

    Use s3 instead of imgur

--- lib/plugins/graphite/graph_view_plugin.rb
+++ lib/plugins/graphite/graph_view_plugin.rb
@@ -1,6 +1,7 @@
 require 'dotenv'
 require 'json'
 require "open-uri"
+require "s3"
 
 require_relative '../../imgur/imgur'
 
@@ -8,7 +9,6 @@ module Slacker
   class GraphViewPlugin
     def initialize(graphite_api)
       @graphite_api = graphite_api
-      @imgur = ENV["IMGUR_CLIENT_ID"] ? Imgur.new(ENV["IMGUR_CLIENT_ID"]) : nil
     end
 
     def ready(robot)
@@ -18,33 +18,35 @@ module Slacker
 
         message << "Okay, here's a graph of `#{graph_id}` for you! :chart_with_upwards_trend:"
 
-        if @imgur.nil?
+        if storage_bucket.nil?
           # If we're not re-hosting images, just send the link directly
           message << graphite_url
         else
-          # Otherwise, do four things, in this order:
+          # Otherwise, do these things things, in this order:
           #   1) download the image data ourselves
-          #   2) upload it to imgur
-          #   3) queue a delete job
-          #   4) send the imgur link into chat
+          #   2) place the data into the bucket
+          #   3) send the bucket-link into chat
+          bucket = storage_bucket
+          img_name = "graphs/#{graph_object_name}.png"
 
-          image_data = open(graphite_url).read
-          image = @imgur.post_image(image_data)
+          object = bucket.objects.build(img_name)
+          object.content = open(graphite_url).read
+          object.save
 
-          # Queue up a job to delete the image after a certain period of time
-          delete_after(image["data"]["deletehash"], ENV["IMGUR_DELETE_AFTER"].to_i)
-
-          message << "https://i.imgur.com/#{image["data"]["id"]}.png"
+          message << "https://s3.amazonaws.com/#{ENV['S3_BUCKET_NANE']}/#{img_name}"
         end
       end
     end
 
     private
-    def delete_after(delete_hash, wait_time)
-      Thread.new do
-        sleep wait_time
-        @imgur.delete_image(delete_hash)
-      end
+    def storage_bucket
+      @s3 ||= S3::Service.new(:access_key_id => ENV['S3_ACCESS_KEY_ID']
+                            :secret_access_key => ENV['S3_SECRET_KEY'])
+      @s3.buckets.find(ENV['S3_BUCKET_NAME'] unless @s3.nil?
+    end
+
+    def graph_object_name
+      return "graph:#{Time.now.to_i}"
     end
   end
 end
