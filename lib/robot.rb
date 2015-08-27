31d183baf8028c888fd314b9e9f57f98cb1f0544
Justin Hennessy <justhennessy@gmail.com>
2015-08-28 07:44:02 +1000

    Changed default DB number to 0

--- lib/robot.rb
+++ lib/robot.rb
@@ -14,7 +14,7 @@ module Slacker
         (name || ENV["NAME"]), [], nil
 
       redis_connection = Redis.new(:host => (ENV["REDIS_HOST"] || "127.0.0.1"),
-                                   :db   => (ENV["REDIS_DB"]   || 15),
+                                   :db   => (ENV["REDIS_DB"]   || 0),
                                    :port => (ENV["REDIS_PORT"] || 6739))
       @redis = Redis::Namespace.new(:ns => :slacker, :redis => redis_connection)
     end
